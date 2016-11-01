4a692e6fa4577beb2712657f6399864c7f611306
evanvosberg <evanvosberg@urban.to>
2016-11-01 23:28:11 +0100

    Add library Q.

--- source/scripts/config.js
+++ source/scripts/config.js
@@ -5,7 +5,8 @@ require.config({
         'react': 'https://cdnjs.cloudflare.com/ajax/libs/react/15.3.2/react',
         'react-dom': 'https://cdnjs.cloudflare.com/ajax/libs/react/15.3.2/react-dom',
         'react-router': 'https://unpkg.com/react-router/umd/ReactRouter',
-        'superagent': 'https://cdnjs.cloudflare.com/ajax/libs/superagent/2.3.0/superagent'
+        'superagent': 'https://cdnjs.cloudflare.com/ajax/libs/superagent/2.3.0/superagent',
+        'q': 'https://cdnjs.cloudflare.com/ajax/libs/q.js/1.1.0/q'
     }
 });
 
