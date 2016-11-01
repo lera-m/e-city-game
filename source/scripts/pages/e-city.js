32aa47a2c851720b55ee63c485a2e304fa02651c
lera-m <buhg.odessa@gmail.com>
2016-11-02 01:12:08 +0200

    superagent request

--- source/scripts/pages/e-city.js
+++ source/scripts/pages/e-city.js
@@ -1,13 +1,41 @@
-define(['react', '../components/side-bar'], function (React, SideBar) {
+define(['react', '../components/side-bar', 'superagent'], function (React, SideBar, Superagent) {
 
     return React.createClass({
 
         displayName: 'E-City',
-
+        
+        getInitialState: function () {
+            return {
+                city: 'Odessa'
+            };
+        },
+        
+        onButtonClick: function (event) {
+            Superagent
+                .get('http://35.160.229.83:8080/city')
+                .set('Accept', 'application/json')
+                .query({
+                    name: this.state.city
+                })
+                .end(function(error, response){
+                    console.log(error, response);
+                });
+        },
+        
+        onInputChange: function (event) {
+            this.setState({
+                city: event.target.value
+            });
+        },
+        
         render: function () {
             return (
                 <div>
                     <SideBar/>
+                    <div>
+                        <input type="text" defaultValue={this.state.city} onChange={this.onInputChange}/>
+                        <button onClick={this.onButtonClick}>Send</button>
+                    </div>
                 </div>
             );
         }
