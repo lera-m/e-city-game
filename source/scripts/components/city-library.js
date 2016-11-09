f6a0d4890a4f6be7dde5deb66538ae8a0b457215
lera-m <buhg.odessa@gmail.com>
2016-11-09 14:01:52 +0200

    all city's names as a list in library

--- source/scripts/components/city-library.js
+++ source/scripts/components/city-library.js
@@ -1,18 +1,41 @@
-define(['react'], function (React) {
+define(['react', 'superagent'], function (React, Superagent) {
     
     return React.createClass ({
         
         displayName: 'City-Library',
         
+        getInitialState: function () {
+            return {
+                library: []
+            };
+        },
+        
+        componentDidMount: function(){
+            this.getLibrary();    
+        },
+        
+        getLibrary: function () {
+            Superagent
+            .get('http://ecity.org.ua:8080/names')
+                .set('Accept', 'application/json')
+                .end((error, response) => /* arrow function */{
+                    this.setState({
+                            library: response.body
+                    });
+                });
+        },
+        
         render: function () {
             return (
                 <div className="city-library">
                     <ul>
-                        <li>Авдеевка — Донецкая область</li>
-                        <li>Александрия — Кировоградская область</li>
-                        <li>Александровск — Луганская область</li>
-                        <li>Алмазная — Луганская область</li>
-                        <li>Алупка — Автономная Республика Крым</li>
+                        {this.state.library && this.state.library.map((city, i) => {
+                            return (
+                                <li key={city.id}>
+                                    <p>{city.name}</p>
+                                </li>
+                            );
+                        })}
                     </ul>
                 </div>
             );
@@ -21,3 +44,4 @@ define(['react'], function (React) {
     });
     
 });
+
