27f87d5f52f55ae082a7cf01ad108553d15664c4
lera-m <buhg.odessa@gmail.com>
2016-12-14 19:19:02 +0200

    regions are shown on the map

--- source/scripts/components/map-leaflet.js
+++ source/scripts/components/map-leaflet.js
@@ -1,26 +1,36 @@
-define(['react', 'leaflet', 'react-dom'], function (React, L, ReactDOM) {
-    
+define(['react', 'leaflet', 'react-dom', './map-leaflet-regions'], function (React, L, ReactDOM, MapLeafletRegions) {
+
     return React.createClass ({
-        
+
         displayName: 'MapLeaflet',
-        
+
         componentDidMount: function(){
             var mapNode = ReactDOM.findDOMNode(this.refs.map);
 
             this.map = L.map(mapNode, {
                 center: [48.6206786, 31.5158776],
-                zoom: 13
+                zoom: 5
             });
+
+            this.updateRegions();
         },
-                
+
+        updateRegions: function () {
+            for (var region in MapLeafletRegions) {
+                var polygon = L.polygon(MapLeafletRegions[region], {color: region === 'odessa' ? 'red' : 'green'});
+
+                polygon.addTo(this.map);
+            }
+        },
+
         render: function () {
             return (
                 <div ref="map" style={{height:'500px'}}>
-                    
+
                 </div>
             );
         }
-        
+
     });
-    
+
 });
