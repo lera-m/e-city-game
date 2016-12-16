3d4f09ec8d8fff77e23c614c32dd143750db41a9
lera-m <buhg.odessa@gmail.com>
2016-12-16 16:51:32 +0200

    new data for regions

---
+++ source/scripts/data/regions.js
@@ -0,0 +1,31 @@
+define(['./Cherkasy', './Chernihiv', './Chernivtsi', './Crimea', './Dnipropetrovsk', './Donetsk', './Ivano-Frankivsk', './Kharkiv', './Kherson', './Khmelnytskyy', './Kiev', './Kirovohrad', './Luhansk', './Lviv', './Mykolayiv', './Odessa', './Poltava', './Rivne', './Sumy', './Ternopil', './Vinnytsya', './Volyn', './Zakarpattia', './Zaporizhzhya', './Zhytomyr'], function (Cherkasy, Chernihiv, Chernivtsi, Crimea, Dnipropetrovsk, Donetsk, IvanoFrankivsk, Kharkiv, Kherson, Khmelnytskyy, Kiev, Kirovohrad, Luhansk, Lviv, Mykolayiv, Odessa, Poltava, Rivne, Sumy, Ternopil, Vinnytsya, Volyn, Zakarpattia, Zaporizhzhya, Zhytomyr) {
+
+    return {
+        Cherkasy, 
+        Chernihiv, 
+        Chernivtsi, 
+        Crimea, 
+        Dnipropetrovsk, 
+        Donetsk, 
+        IvanoFrankivsk, 
+        Kharkiv, 
+        Kherson, 
+        Khmelnytskyy, 
+        Kiev, 
+        Kirovohrad, 
+        Luhansk, 
+        Lviv, 
+        Mykolayiv, 
+        4: Odessa, 
+        Poltava, 
+        Rivne, 
+        Sumy, 
+        Ternopil, 
+        Vinnytsya, 
+        Volyn, 
+        Zakarpattia, 
+        Zaporizhzhya, 
+        Zhytomyr
+    };
+
+});
