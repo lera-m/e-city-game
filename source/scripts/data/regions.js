2810cc05e4bca3695550feabb168cbca28b9545e
lera-m <buhg.odessa@gmail.com>
2016-12-16 17:21:56 +0200

    region’s ids were added

--- source/scripts/data/regions.js
+++ source/scripts/data/regions.js
@@ -1,31 +1,57 @@
 define(['./Cherkasy', './Chernihiv', './Chernivtsi', './Crimea', './Dnipropetrovsk', './Donetsk', './Ivano-Frankivsk', './Kharkiv', './Kherson', './Khmelnytskyy', './Kiev', './Kirovohrad', './Luhansk', './Lviv', './Mykolayiv', './Odessa', './Poltava', './Rivne', './Sumy', './Ternopil', './Vinnytsya', './Volyn', './Zakarpattia', './Zaporizhzhya', './Zhytomyr'], function (Cherkasy, Chernihiv, Chernivtsi, Crimea, Dnipropetrovsk, Donetsk, IvanoFrankivsk, Kharkiv, Kherson, Khmelnytskyy, Kiev, Kirovohrad, Luhansk, Lviv, Mykolayiv, Odessa, Poltava, Rivne, Sumy, Ternopil, Vinnytsya, Volyn, Zakarpattia, Zaporizhzhya, Zhytomyr) {
 
     return {
-        Cherkasy, 
-        Chernihiv, 
-        Chernivtsi, 
-        Crimea, 
-        Dnipropetrovsk, 
-        Donetsk, 
-        IvanoFrankivsk, 
-        Kharkiv, 
-        Kherson, 
-        Khmelnytskyy, 
-        Kiev, 
-        Kirovohrad, 
-        Luhansk, 
-        Lviv, 
-        Mykolayiv, 
+        1: Zaporizhzhya, 
+        2: Dnipropetrovsk, 
+        3: Kherson, 
         4: Odessa, 
-        Poltava, 
-        Rivne, 
-        Sumy, 
-        Ternopil, 
-        Vinnytsya, 
-        Volyn, 
-        Zakarpattia, 
-        Zaporizhzhya, 
-        Zhytomyr
+        5: Crimea, 
+        6: Mykolayiv,
+        7: Kirovohrad, 
+        8: Donetsk,
+        9: Luhansk, 
+        10: Kharkiv, 
+        11: Sumy, 
+        12: Poltava,
+        13: Cherkasy,
+        14: Vinnytsya,
+        15: Chernihiv,
+        16: Kiev,
+        17: Zhytomyr,
+        18: Khmelnytskyy, 
+        19: Chernivtsi, 
+        20: Ternopil,
+        21: Rivne, 
+        22: Volyn, 
+        23: IvanoFrankivsk, 
+        24: Lviv, 
+        25: Zakarpattia
     };
 
 });
+
+ 
+         
+        
+        
+        
+         
+        
+        
+        
+        
+         
+        
+        
+       
+         
+        
+         
+        
+        
+         
+         
+        
+         
+        
+        
