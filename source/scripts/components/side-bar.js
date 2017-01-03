2635072faf753f145b4d6aac0991b17c5832cbd3
lera-m <buhg.odessa@gmail.com>
2017-01-03 18:01:07 +0200

    popup was added and styles for it

--- source/scripts/components/side-bar.js
+++ source/scripts/components/side-bar.js
@@ -1,68 +1,88 @@
-define(['react', 'superagent', '../settings', 'react-popup'], function (React,  Superagent, Settings, Popup) {
+/*global require, module*/
+
+const React = require('react');
+const ReactDom = require('react-dom');
+const Superagent = require('superagent');
+const Popup = require('react-popup').default;
+const Settings = require('../settings');
+
+const SideBar = React.createClass({
+
+    displayName: 'SideBar',
+
+    propTypes: {
+        game: React.PropTypes.object.isRequired
+    },
     
-    return React.createClass ({
+    componentDidMount: function () {
+        if (!document.getElementById('popupContainer')) {
+            const popupContainer = document.createElement('div');
+            
+            popupContainer.id = 'popupContainer';
+            
+            document.body.appendChild(popupContainer);
         
-        displayName: 'SideBar',
-        
-        propTypes: {
-            game: React.PropTypes.object.isRequired
-        },
-        
-/*
-        onButtonClick: function(event) {
-            Popup.create({
-                content: 'Вы уверены, что хотите начать новую игру?',
-                buttons: {
-                    left: ['нет'],
-                    right: [{
-                        text: 'да',
-                        action: function(popup){
-                            this.props.game.getGameId()
-                                .then(() => {
-                                    location.href="#/e-city";
-                                });
-                            popup.close();
-                        }
-                    }]
-                },
-            });
-*/
-        onButtonClick: function(event) {
-            this.props.game.getGameId()
-                .then(() => {
-                });
-        },
-        
-        render: function () {
-            return (
-                <div className="side-bar bg-color">
-                    <div className="logo">
-                        <p>E-City</p>
-                    </div>
-                    <div>
-                        <a className='text-control-color' href='#/e-city' onClick={this.onButtonClick}>Новая игра</a>
-                    </div>
-                    <div>
-                        <a className='grey-color' href="#">Продолжить</a>
-                    </div>
-                    <div>
-                        <a className='text-control-color' href="#/e-city">Рекорды</a>
-                    </div>
-                    <div>
-                        <a className='text-control-color' href="#/rules">Правила</a>
-                    </div>
-                    <div>
-                        <a className='text-control-color' href="#/library">Библиотека</a>
-                    </div>
-                    <div>
-                        <a className='text-control-color' href="#/before-start">Выход</a>
-                    </div>
-                    <div>
-                    </div>
-                </div>
-            );
+            ReactDom.render(<Popup/>, popupContainer);
         }
+    },
+
+    onButtonClick: function(event) {
+        event.preventDefault();
         
-    });
+        Popup.create({
+            content: 'Вы уверены, что хотите начать новую игру?',
+            buttons: {
+                left: [{
+                    text: 'нет',
+                    action: (popup) => {
+                        popup.close();
+                    }
+                }],
+                right: [{
+                    text: 'да',
+                    action: (popup) => {
+                        this.props.game.getGameId()
+                            .then(() => {
+                                console.log('popup action');
+                                location.href="#/e-city";
+                            });
+                        popup.close();
+                    }
+                }]
+            }
+        });
+    },
     
+    render: function () {
+        return (
+            <div className="side-bar bg-color">
+                <div className="logo">
+                    <p>E-City</p>
+                </div>
+                <div>
+                    <a className='text-control-color' href='#' onClick={this.onButtonClick}>Новая игра</a>
+                </div>
+                <div>
+                    <a className='grey-color' href="#">Продолжить</a>
+                </div>
+                <div>
+                    <a className='text-control-color' href="#/e-city">Рекорды</a>
+                </div>
+                <div>
+                    <a className='text-control-color' href="#/rules">Правила</a>
+                </div>
+                <div>
+                    <a className='text-control-color' href="#/library">Библиотека</a>
+                </div>
+                <div>
+                    <a className='text-control-color' href="#/before-start">Выход</a>
+                </div>
+                <div>
+                </div>
+            </div>
+        );
+    }
+
 });
+
+module.exports = SideBar;
