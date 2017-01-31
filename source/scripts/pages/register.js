7e0e6f8305058d3e40f2059c6d913fd8e96109a6
lera-m <buhg.odessa@gmail.com>
2017-01-31 16:21:20 +0200

    create form around

--- source/scripts/pages/register.js
+++ source/scripts/pages/register.js
@@ -38,7 +38,7 @@ define(['react', 'superagent', '../settings'], function (React, Superagent, Sett
             }
         },
         
-        onButtonClick: function (event) {
+        onFormSubmit: function (event) {
             var login = this.state.login,
             password = this.state.password,
             email =  this.emailValidation(this.state.email),
@@ -97,30 +97,32 @@ define(['react', 'superagent', '../settings'], function (React, Superagent, Sett
                         <div className="logo">
                             <p>E-City</p>
                         </div>
-                        <div>
-                            <input type="text" className='register_input_style' placeholder="Логин" onChange={this.onInputChange.bind(this, 'login')}/>
-                        </div>
-                        <div>
-                            <input type="password" className='register_input_style' placeholder="Пароль" onChange={this.onInputChange.bind(this, 'password')}/>
-                        </div>
-                        <div>
-                            <input type="email" className='register_input_style' placeholder="Email" onChange={this.onInputChange.bind(this, 'email')}/>
-                        </div>
-                        <div>
-                            <input type="text" className='register_input_style' placeholder="Имя(опционально)" onChange={this.onInputChange.bind(this, 'name')}/>
-                        </div>
-                        <div>
-                            <input type="text" className='register_input_style' placeholder="Фамилия(опционально)" onChange={this.onInputChange.bind(this, 'surname')}/>
-                        </div>
-                        <div>
-                            <input type="text" className='register_input_style' placeholder="Город(опционально)"onChange={this.onInputChange.bind(this, 'city')}/>
-                        </div>
-                        <div className='warning-message'>
-                            {this.state.warningMessage}
-                        </div>
-                        <div>
-                            <input type="submit" className='register_submit' onClick={this.onButtonClick} value="Зарегистрироваться"/>
-                        </div>
+                        <form onSubmit={this.onFormSubmit}>
+                            <div>
+                                <input type="text" className='register_input_style' placeholder="Логин" onChange={this.onInputChange.bind(this, 'login')}/>
+                            </div>
+                            <div>
+                                <input type="password" className='register_input_style' placeholder="Пароль" onChange={this.onInputChange.bind(this, 'password')}/>
+                            </div>
+                            <div>
+                                <input type="email" className='register_input_style' placeholder="Email" onChange={this.onInputChange.bind(this, 'email')}/>
+                            </div>
+                            <div>
+                                <input type="text" className='register_input_style' placeholder="Имя(опционально)" onChange={this.onInputChange.bind(this, 'name')}/>
+                            </div>
+                            <div>
+                                <input type="text" className='register_input_style' placeholder="Фамилия(опционально)" onChange={this.onInputChange.bind(this, 'surname')}/>
+                            </div>
+                            <div>
+                                <input type="text" className='register_input_style' placeholder="Город(опционально)"onChange={this.onInputChange.bind(this, 'city')}/>
+                            </div>
+                            <div className='warning-message'>
+                                {this.state.warningMessage}
+                            </div>
+                            <div>
+                                <input type="submit" className='register_submit' value="Зарегистрироваться"/>
+                            </div>
+                        </form>
                     </div>
                 </div>  
             );
